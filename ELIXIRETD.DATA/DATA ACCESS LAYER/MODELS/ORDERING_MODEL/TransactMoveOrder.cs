20d42c22182c04f0dd61a7bc959770248601e377
PhilipCrasco <119844219+PhilipCrasco@users.noreply.github.com>
2023-03-10 08:28:40 +0800

    add delivery status

--- ELIXIRETD.DATA/DATA ACCESS LAYER/MODELS/ORDERING_MODEL/TransactMoveOrder.cs
+++ ELIXIRETD.DATA/DATA ACCESS LAYER/MODELS/ORDERING_MODEL/TransactMoveOrder.cs
@@ -19,6 +19,8 @@
 
         public bool IsTransact { get; set; }
 
+        public string DeliveryStatus { get; set; }
+
         
     }
 }
---
+++ ELIXIRETD.DATA/Migrations/20230310002731_createdeliveryStatus.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ELIXIRETD.DATA.Migrations
+{
+    public partial class createdeliveryStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DeliveryStatus",
+                table: "TransactOrder",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeliveryStatus",
+                table: "TransactOrder");
+        }
+    }
+}
